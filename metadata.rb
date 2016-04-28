5c6e329a01c3f0b251df812697a303977c94136d
David Higgins <david@gryphonandrook.com>
2016-04-28 15:52:48 -0600

    Initial Commit

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+name             'gnr_whenever'
+maintainer       'Gryphon & Rook Inc'
+license          'All rights reserved'
+description      'Configures whenever'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
